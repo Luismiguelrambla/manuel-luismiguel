b95fdd26454bb83baef71dd99d341629227882be
Manuel Alejandro <73426945+manuelalejandroiris@users.noreply.github.com>
2021-02-19 16:50:15 +0100

    Add files via upload

---
+++ frontend/uploadimg_manu/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "./App.css";
+export class App extends Component {
+  state = {
+    profileImg: "User-Avatar.svg",
+  };
+  imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        this.setState({ profileImg: reader.result });
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+  render() {
+    const { profileImg } = this.state;
+    return (
+      <div className="page">
+        <div className="container">
+          <h1 className="heading">My Profile</h1>
+          <div className="img-holder">
+            <img src={profileImg} alt="" id="img" className="img" />
+          </div>
+          <input
+            type="file"
+            accept="image/*"
+            name="image-upload"
+            id="input"
+            onChange={this.imageHandler}
+          />
+          <div className="label">
+            <label className="image-upload" htmlFor="input">
+              <i className="material-icons">add_photo_alternate</i>
+              Choose your Photo
+            </label>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
